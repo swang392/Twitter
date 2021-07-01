cdb516ea172c89b8330817786a3ba47c8f48517d
Sarah Wang <sarah.wang2009@gmail.com>
2021-07-01 16:21:27 -0400

    tweet character count

--- View Controllers/ComposeViewController.m
+++ View Controllers/ComposeViewController.m
@@ -9,8 +9,11 @@
 #import "ComposeViewController.h"
 #import "APIManager.h"
 
-@interface ComposeViewController ()
+@interface ComposeViewController () <UITextViewDelegate>
+
 @property (weak, nonatomic) IBOutlet UITextView *postView;
+@property (weak, nonatomic) IBOutlet UILabel *characterCountLabel;
+
 
 @end
 
@@ -20,6 +23,11 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view.
+    
+    self.postView.delegate = self;
+    self.postView.layer.borderWidth = 2.0f;
+    self.postView.layer.borderColor = [[UIColor darkGrayColor] CGColor];
+    self.postView.layer.cornerRadius = 8;
 }
 - (IBAction)didTweet:(UIBarButtonItem *)sender {
     [[APIManager shared] postStatusWithText:(self.postView.text) completion:^(Tweet * tweet, NSError * error){
@@ -39,14 +47,22 @@
     [self dismissViewControllerAnimated:true completion:nil];
 }
 
-/*
-#pragma mark - Navigation
-
-// In a storyboard-based application, you will often want to do a little preparation before navigation
-- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
-    // Get the new view controller using [segue destinationViewController].
-    // Pass the selected object to the new view controller.
+- (BOOL)textView:(UITextView *)textView shouldChangeTextInRange:(NSRange)range replacementText:(NSString *)text{
+    
+    int characterLimit = 140;
+    
+    NSString *newText = [self.postView.text stringByReplacingCharactersInRange:range withString:text];
+    
+    if(newText.length >= characterLimit)
+    {
+        self.characterCountLabel.text = @"exceeded maximum characters";
+    }
+    else {
+        self.characterCountLabel.text = [NSString stringWithFormat:@"%d", newText.length];
+    }
+    
+    return newText.length < characterLimit;
 }
-*/
-
-@end
+ 
+@end 
+ 
